7a8778c4a4faaeaec27ef1b83e8cbb3a0d1a550e
Christoph Rueff <christoph.rueff@gmail.com>
2019-04-11 14:37:31 +0200

    Add javadoc to the main class

--- ResMailPrank/src/main/java/ch/heigvd/res/mailbot/ResMailPrank.java
+++ ResMailPrank/src/main/java/ch/heigvd/res/mailbot/ResMailPrank.java
@@ -9,6 +9,17 @@ import java.io.IOException;
 import java.util.List;
 import java.util.logging.Logger;
 
+/**
+ * Class ResMailPrank
+ *
+ * Main class of the application.
+ * Forms groups of people to play a prank on by choosing one
+ * of the group member to be the sender and the other to be the victims
+ * that will receive a message from a given message list
+ *
+ * @author Jostoph
+ * @version 1.0
+ */
 public class ResMailPrank {
 
     private static Logger LOGGER = Logger.getLogger(ResMailPrank.class.getName());
@@ -16,12 +27,17 @@ public class ResMailPrank {
     public static void main(String[] args) {
 
         try {
+            // get the configuration for the prank
             ConfigManager  configManager = new ConfigManager();
+            // creates a new SMTP client
             SmtpClient client = new SmtpClient(configManager.getServerAddress(), configManager.getServerPort());
+            // creates a new PrankGenerator with the configManager
             PrankGenerator generator = new PrankGenerator(configManager);
 
+            // generate the list of pranks
             List<Prank> pranks = generator.generatePranks();
 
+            // executing the pranks by sending the mails using the SMTP client
             for (Prank prank : pranks) {
                 client.sendMail(prank.getMail());
             }
