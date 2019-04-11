806229b249d56e9598beee3014348c06f075e6a8
Christoph Rueff <christoph.rueff@gmail.com>
2019-04-11 14:20:59 +0200

    Add javadoc to the model.prank classes, correct typo in PrankGenerator

--- ResMailPrank/src/main/java/ch/heigvd/res/mailbot/model/prank/Prank.java
+++ ResMailPrank/src/main/java/ch/heigvd/res/mailbot/model/prank/Prank.java
@@ -3,9 +3,20 @@ package ch.heigvd.res.mailbot.model.prank;
 import ch.heigvd.res.mailbot.model.mail.Group;
 import ch.heigvd.res.mailbot.model.mail.Mail;
 
+/**
+ * Class Prank
+ *
+ * Class representing a prank,
+ * it is composed of a group to prank and a message to send
+ *
+ * @author Jostoph
+ * @version 1.0
+ */
 public class Prank {
 
+    // to group of Person (people) to prank
     private Group group;
+    // the message to send to the victims of the group
     private String message;
 
     public Prank(Group group, String message) {
@@ -13,6 +24,9 @@ public class Prank {
         this.message = message;
     }
 
+    /**
+     * @return a prank Mail from the group's sender to the victims with the message to send
+     */
     public Mail getMail() {
         return new Mail(group.getSender(), group.getVictims(), message);
     }
--- ResMailPrank/src/main/java/ch/heigvd/res/mailbot/model/prank/PrankGenerator.java
+++ ResMailPrank/src/main/java/ch/heigvd/res/mailbot/model/prank/PrankGenerator.java
@@ -10,34 +10,66 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Logger;
 
+/**
+ * Class PrankGenerator
+ *
+ * Class representing a generator of prank lists
+ *
+ * @author Jostoph
+ * @version 1.0
+ *
+ * Inspired by the Labo SMTP, part 4 video from professor Olivier Liechti
+ * in the context of the RES course at the heig-vd
+ *
+ * https://www.youtube.com/watch?v=OrSdRCt_6YQ
+ */
 public class PrankGenerator {
 
     private static final Logger LOGGER = Logger.getLogger(PrankGenerator.class.getName());
 
     private ConfigManager configManager;
 
+    /**
+     * Constructor
+     *
+     * @param configManager the configuration manager to generate the pranks
+     */
     public PrankGenerator(ConfigManager configManager) {
         this.configManager = configManager;
     }
 
+    /**
+     * generate Pranks with generated groups of Person (people) from a randomly sorted list
+     * and a message selected from a randomly sorted list of messages, respecting a given number of
+     * groups if possible
+     *
+     * @return a list of Pranks ready to be performed
+     */
     public List<Prank> generatePranks() {
+        // the list of groups
         List<Group> groups;
+        // the list of pranks to generate
         List<Prank> pranks;
 
         int numberOfPeople = configManager.getPeople().size();
         int numberOfGroups = configManager.getNumberOfGroups();
         List<String> messages = configManager.getMessages();
+        // shuffling the messages
         Collections.shuffle(messages);
 
+        // if there are less than 3 people per group with the given number of groups
+        // the number of groups is set so that there are at least 3 people per group
         if((numberOfPeople / numberOfGroups) < 3) {
             numberOfGroups = numberOfPeople / 3;
             LOGGER.warning("The size of the groups to generate the pranks is to small (need at least 3 people), it has been set to : " + numberOfGroups);
         }
 
+        // generation of the list of groups
         groups = generateGroups(configManager.getPeople(), numberOfGroups);
 
         pranks = new ArrayList<>();
 
+        // setting a message for each group
         int messageIndex = 0;
         for(Group group : groups) {
             pranks.add(new Prank(group, messages.get(messageIndex)));
@@ -47,8 +79,16 @@ public class PrankGenerator {
         return pranks;
     }
 
+    /**
+     * private helper method to generate the groups for the pranks
+     *
+     * @param people the list of Person (people)
+     * @param numberOfGroups the number of groups to generate
+     * @return the list of created groups
+     */
     private List<Group> generateGroups(List<Person> people, int numberOfGroups) {
         List<Group> groups = new ArrayList<>();
+        // list of randomly sorted Person (people)
         LinkedList<Person> randomSortedPeople = new LinkedList<>(people);
         Collections.shuffle(randomSortedPeople);
 
@@ -61,11 +101,11 @@ public class PrankGenerator {
         }
 
         // spread victims in the groups
-        int groupeIndex = 0;
+        int groupIndex = 0;
         while (!randomSortedPeople.isEmpty()) {
-            groups.get(groupeIndex).addVictim(randomSortedPeople.getFirst());
+            groups.get(groupIndex).addVictim(randomSortedPeople.getFirst());
             randomSortedPeople.removeFirst();
-            groupeIndex = (groupeIndex + 1) % numberOfGroups;
+            groupIndex = (groupIndex + 1) % numberOfGroups;
         }
         return groups;
     }
