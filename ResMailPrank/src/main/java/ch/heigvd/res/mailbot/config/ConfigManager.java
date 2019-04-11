40adf62e4c53aba12aad563c6ab28ac7132dcc5f
Christoph Rueff <christoph.rueff@gmail.com>
2019-04-11 13:56:02 +0200

    Add javadoc in the config management classes

--- ResMailPrank/src/main/java/ch/heigvd/res/mailbot/config/ConfigManager.java
+++ ResMailPrank/src/main/java/ch/heigvd/res/mailbot/config/ConfigManager.java
@@ -8,6 +8,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+/**
+ * Class ConfigManager
+ *
+ * Concrete implementation of a configuration manager using the IConfigManager interface
+ * Retrieves the data in the configuration files in order to create the objects needed to
+ * generate mail pranks
+ *
+ * @author Jostoph
+ * @version 1.0
+ */
 public class ConfigManager implements IConfigManager {
 
     // properties
@@ -19,6 +29,12 @@ public class ConfigManager implements IConfigManager {
     private List<Person> people;
     private List<String> messages;
 
+    /**
+     * Constructor
+     *
+     * @throws IOException if the there is an error while parsing the files
+     * @throws IllegalArgumentException if the data doesn't permit to generate a prank
+     */
     public ConfigManager() throws IOException, IllegalArgumentException {
         people = loadPeople("./config/people.utf8");
         messages = loadMessages("./config/messages.utf8");
@@ -58,6 +74,13 @@ public class ConfigManager implements IConfigManager {
         return port;
     }
 
+    /**
+     * loads the Person (people) from the people configuration file
+     *
+     * @param path the path to the file
+     * @return the list of Person (people) created from the parsed email addresses
+     * @throws IOException if there is an error while parsing or opening the file
+     */
     private List<Person> loadPeople(String path) throws IOException {
 
         List<Person> people = new ArrayList<>();
@@ -74,9 +97,18 @@ public class ConfigManager implements IConfigManager {
         return people;
     }
 
+    /**
+     * loads the messages from the messages configuration file
+     *
+     * @param path the path to the file
+     * @return the list of messages that will be available to create the prank mails (String)
+     * @throws IOException if there is an error while parsing or opening the file
+     */
     private List<String> loadMessages(String path) throws IOException {
 
         List<String> messages = new ArrayList<>();
+
+        // "---" is the separator between messages
         String messageSeparator = "---";
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(path), StandardCharsets.UTF_8));
@@ -94,11 +126,16 @@ public class ConfigManager implements IConfigManager {
             line = reader.readLine();
         }
 
-
         reader.close();
         return messages;
     }
 
+    /**
+     * loads the properties from the configuration properties file
+     *
+     * @param path the path to the properties file
+     * @throws IOException if there is an error while parsing or opening the file
+     */
     private void loadProperties(String path) throws IOException {
         FileInputStream inputStream = new FileInputStream(path);
         Properties properties = new Properties();
--- ResMailPrank/src/main/java/ch/heigvd/res/mailbot/config/IConfigManager.java
+++ ResMailPrank/src/main/java/ch/heigvd/res/mailbot/config/IConfigManager.java
@@ -4,15 +4,39 @@ import ch.heigvd.res.mailbot.model.mail.Person;
 
 import java.util.List;
 
+/**
+ * Interface IConfigManager
+ *
+ * Interface that provides methods to retrieve the data from the
+ * configuration files
+ *
+ * @author Jostoph
+ * @version 1.0
+ */
 public interface IConfigManager {
 
+    /**
+     * @return the list of Person (people) from the configuration files
+     */
     List<Person> getPeople();
 
+    /**
+     * @return the number of groups set in the properties
+     */
     int getNumberOfGroups();
 
+    /**
+     * @return the list of messages from the configuration files
+     */
     List<String> getMessages();
 
+    /**
+     * @return the SMTP server address from the properties
+     */
     String getServerAddress();
 
+    /**
+     * @return the SMTP server port from the properties
+     */
     int getServerPort();
 }
